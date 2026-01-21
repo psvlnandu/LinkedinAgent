4975fbbb1dcb686f0ee2d3403c60d5bd7f4d88aa
poorna <raavip@clarkson.edu>
2026-01-21 14:46:05 -0600

    Remove unused imports in MainActivity

--- LinkedinAgent/app/src/main/java/com/example/linkedinagent/MainActivity.kt
+++ LinkedinAgent/app/src/main/java/com/example/linkedinagent/MainActivity.kt
@@ -16,10 +16,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
